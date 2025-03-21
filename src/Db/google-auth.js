3167a255839af25363c3d8d0d15df711bfdd43da
swatibersurda <swatibersurda@gmail.com>
2025-03-21 17:51:45 +0530

    add

--- src/Db/google-auth.js
+++ src/Db/google-auth.js
@@ -1,36 +1,43 @@
 // config/passport.js
-import passport from 'passport';
-import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
-import  {v4 as uuid} from "uuid"
+import passport from "passport";
+import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+import { v4 as uuid } from "uuid";
 // import User from '../models/user.js';
-import dotenv from 'dotenv';
-import { User } from '../Model/userModel.js';
-dotenv.config({path:"./.env"});
-passport.use(new GoogleStrategy({
-  clientID: process.env.CLIENT_ID,
-  clientSecret: process.env.CLIENT_SECRET,
-  // this url is given on redirect timeee..
-  callbackURL: "http://localhost:9000/api/v1/google/auth/google/callback",
-  // callbackURL: "https://ecommdoorstep.onrender.com/api/v1/google/auth/google/callback",
-}, async (accessToken, refreshToken, profile, done) => {
-  try {
-   console("hi nn")
-    // return done(null, user);
-    let user=await User.findOne({email:profile?._json.email})
-    // will create the user.
-    if(!user){
-      user=await User.create({
-        name:profile._json.name,
-        email:profile?._json.email,
-        password:uuid(),
-
-      })
+import dotenv from "dotenv";
+import { User } from "../Model/userModel.js";
+dotenv.config({ path: "./.env" });
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      // this url is given on redirect timeee..
+      // callbackURL: "http://localhost:9000/api/v1/google/auth/google/callback",
+      callbackURL: "https://ecommdoorstep.onrender.com/api/v1/google/auth/google/callback",
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        console.log(profile,"i am profile")
+        // console("hi nn");
+        // return done(null, user);
+        let user = await User.findOne({ email: profile?._json.email });
+        // console.log(user,"i am userr")
+        // will create the user.
+        if (!user) {
+          user = await User.create({
+            name: profile._json.name,
+            email: profile?._json.email,
+            password: uuid(),
+          });
+        }
+        return done(null, user);
+      } catch (err) {
+        console.log(err,"i am")
+        return done(err, false);
+      }
     }
-    return done(null,user)
-  } catch (err) {
-    return done(err, false);
-  }
-}));
+  )
+);
 
 // Serialize user into session
 passport.serializeUser((user, done) => {
